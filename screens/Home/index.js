f6b2aea690707ca80c96ddf6a3320caa3646490e
Hazrul <hal1993@hotmail.com>
2023-12-26 22:08:23 +0800

    Add calculation

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,13 +1,22 @@
-import { Dimensions, StyleSheet, Text, View } from "react-native"
+import { Dimensions, Pressable, StyleSheet, Text, TextInput, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { EvilIcons } from '@expo/vector-icons';
 import PieChart from "react-native-pie-chart";
+import React from "react";
+import { AntDesign } from '@expo/vector-icons';
 
 const Home = () => {
 
     const widthAndHeight = 250
     const series = [120,180]
     const sliceColor = ['pink', 'white']
+
+    const [money, setMoney] = React.useState(1000)
+    const [cash, setCash] = React.useState("")
+
+    const handleChangeSavings = () => {
+        setMoney((money) => money + parseInt(cash))
+    }
 
     return (
         <SafeAreaView>
@@ -21,7 +30,7 @@ const Home = () => {
                 </View>
             </View>
             <View style={style.middleContainer}>
-            <Text style={{color: 'pink', fontSize: 40, fontWeight: 'bold', position: 'absolute', zIndex: 1}}>$1000</Text>
+            <Text style={style.money}>${money}</Text>
             <PieChart
             widthAndHeight={widthAndHeight}
             series={series}
@@ -29,6 +38,10 @@ const Home = () => {
             coverRadius={0.7}
             coverFill={'white'}
           />
+            </View>
+            <View style={style.middleSecondContainer}>
+                <TextInput value={cash} onChangeText={setCash} style={style.input} placeholder="Input cash"/>
+                <Pressable onPress={() => handleChangeSavings()}><AntDesign name="plus" size={24} color="black" /></Pressable>
             </View>
             <View style={style.bottomContainer}>
 
@@ -41,6 +54,18 @@ const Home = () => {
 export default Home
 
 const style = StyleSheet.create({
+    input: {
+        width: Dimensions.get('screen').width/2,
+        backgroundColor: 'white',
+        padding: 10,
+        marginRight: 10
+    },
+    middleSecondContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    money: {color: 'pink', fontSize: 40, fontWeight: 'bold', position: 'absolute', zIndex: 1},
     middleContainer: {
         justifyContent: 'center',
         alignItems: "center",
